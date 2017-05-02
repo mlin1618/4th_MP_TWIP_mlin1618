32245bc230a31a296a30bf27f03a56e98d2d18bc
ml996 <ml996@pvsd.org>
2017-05-02 14:28:44 -0400

    ..

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static void SOP(Object s){
+        System.out.println(s);
+    }
     public static double getPoints(String s){
         Scanner sc = new Scanner(s);
         double d = 0;
@@ -19,15 +22,64 @@ public class Main {
         return d;
     }
     public static void main(String[] args) throws IOException{
-        double[][] data  = new double[28][4]; //secret number, total avg, fr1, fr2
+        double[][] data = new double[14][8]; // 0secret number, 1total avg, 2total1, 3fr1, 4fr2, 5total2, 6fr1, 7fr2
         Scanner sc = new Scanner(new File("rawDataU6.txt"));
-        /*
+        sc.nextLine();
+        for(double j = 0; j < 4; j++){
+            int i = (int)(j/2);
+            int c = 0;
+            if(j % 2 != 0){
+                c = 3;
+            }
+            data[i][0] = sc.nextDouble();
+            sc.useDelimiter("\t");
+            String fr1A = sc.next();
+            String fr1B = sc.next();
+            String fr1Syntax = sc.next();
+            sc.next();
+            String fr2A = sc.next();
+            String fr2B = sc.next();
+            String fr2C = sc.next();
+            String fr2Syntax = sc.next();
+            sc.nextLine();
+
+            //FR1
+            data[i][3+c] += getPoints(fr1A) + getPoints(fr1B);
+            if(!fr1Syntax.isEmpty())
+                data[i][3+c] -= Double.parseDouble(fr1Syntax) * 0.25;
+            //FR2
+            data[i][4+c] += getPoints(fr2A) + getPoints(fr2B);
+            if(!fr2Syntax.isEmpty())
+                data[i][4+c] -= Double.parseDouble(fr2Syntax) * 0.25;
+            //Total
+            data[i][2+c] = data[i][3+c] + data[i][4+c];
+        }
+        for(int i = 0; i < data.length; i++){
+            data[i][1] = (data[i][2] + data[i][5])/2;
+        }
+        for(int i = 0; i < data.length; i++){
+            System.out.println(data[i][0] + "\t"
+                    + String.valueOf(data[i][1]) + "/19" + "\t"
+                    + data[i][2] + "\t"
+                    + data[i][5]);
+        }
+    }
+
+
+    /*public static void main(String[] args) throws IOException{
+        double[][] data  = new double[14][8]; //secret number, total avg, total 1, fr1, fr2, total 2, fr 1, fr 2
+        Scanner sc = new Scanner(new File("rawDataU6.txt"));
+
         secret number,
-        */
+
         //just delete #12
         sc.nextLine();
-        for(double j = 0; j < 2; j++) {
+        for(double j = 0; j < 6; j++) {
             int i = (int)j;
+            int c = 0;
+            if(j % 2 == 1){
+                c = 3;
+            }
             data[i][0] = sc.nextDouble();
             sc.useDelimiter("\t");
             String fr1A = sc.next();
@@ -40,21 +92,26 @@ public class Main {
             String fr2Syntax = sc.next();
             sc.nextLine();
             //FR1
-            data[i][2] += getPoints(fr1A) + getPoints(fr1B);
+            data[i][2+c] += getPoints(fr1A) + getPoints(fr1B);
             if(!fr1Syntax.isEmpty())
-                data[i][2] -= Double.parseDouble(fr1Syntax) * 0.25;
+                data[i][2+c] -= Double.parseDouble(fr1Syntax) * 0.25;
             //FR2
-            data[i][3] += getPoints(fr2B) + getPoints(fr2B);
+            data[i][3+c] += getPoints(fr2A) + getPoints(fr2B);
             if(!fr2Syntax.isEmpty())
-                data[i][3] -= Double.parseDouble(fr2Syntax) * 0.25;
+                data[i][3+c] -= Double.parseDouble(fr2Syntax) * 0.25;
             //Total
-            data[i][1] = (data[i][2] + data[i][3])/2;
+            data[i][2+c] = data[i][3+c] + data[i][4+c];
+
+        }
+        for(int i = 0; i < data.length; i++){
+            data[i][1] = (data[i][2] + data[i][5])/2;
         }
         for(int i = 0; i < data.length; i++){
             System.out.println(data[i][0] + "\t"
                                 + String.valueOf(data[i][1]) + "/19" + "\t"
                                 + data[i][2] + "\t"
-                                + data[i][3]);
+                                + data[i][5]);
         }
-    }
+    }*/
+
 }
